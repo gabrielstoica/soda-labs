99e769e1ee752cd94b8c709121fedfe8b9e1fee8
gabrielstoica <marius_gabriel1998@yahoo.com>
2024-04-01 09:21:25 +0300

    docs: update deployment script docs

--- script/StringStorage.sol
+++ script/StringStorage.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import { BaseScript } from "./Base.s.sol";
 import "../src/StringStorage.sol";
 
-// Run this script by "forge script script/StringStorage.s.sol --rpc-url sepolia"
+// Run this script by "forge script script/StringStorage.s.sol --fork-url http://127.0.0.1:8545"
 contract StringStorageScript is BaseScript {
     function run() public broadcast returns (StringStorage stringStorage) {
         // Deploy the the {StringStorage} contract instance
